52667366a7eeab6d4df4d6bbf528d39f58551d0b
DanXSpace <danielleeholung@gmail.com>
2026-06-08 14:50:51 -0400

    Add: path-sum - submission-3

---
+++ Data Structures & Algorithms/path-sum/submission-3.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        
+        def backtracking(node, pathSum):
+            if node is None:
+                return False
+            
+            pathSum += int(node.val)
+
+            if node.left is None and node.right is None:
+                return pathSum == targetSum
+            
+            if backtracking(node.left, pathSum):
+                return True
+            
+            if backtracking(node.right, pathSum):
+                return True
+            return False
+
+        return backtracking(root, 0)
